a182874d80dbefca48dfb187427a25c1704078a7
Mohamed Gamal Youssef <Generaljemmy.mg@gmail.com>
2019-12-16 20:45:06 +0200

    try it playing audio!

---
+++ client1_c/client1_c/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;// Socket programming library
+using System.IO;
+
+namespace client1_c
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpClient _client = new TcpClient();// Provides client connections for TCP network services.
+                _client.Connect("192.168.1.5", 8888);
+                NetworkStream _ns = _client.GetStream();// connect to the media/ stream/ link 
+
+                /* FOR IMAGE note using below function */
+                //StreamReader arrSize = new StreamReader(_ns);
+                //int imgSize = int.Parse(arrSize.ReadLine());
+                //byte[] _imgArray = new byte[imgSize];
+                //_ns.Read(_imgArray, 0, _imgArray.Length);
+                //Image img = byteArrayToImage(_imgArray);
+                //img.Save("C:\\Users\\JemmY_NeutroN\\Desktop\\img.jpeg");
+                //MessageBox.Show("Image has been saved ", "Client");
+
+                /* FOR PATHS of images  i.e. array of text */ 
+                //StreamWriter _sw = new StreamWriter(_ns);
+                //_sw.WriteLine("C:\\Users\\JemmY_NeutroN\\Desktop\\photos\\");
+                //_sw.Flush();
+                //StreamReader _num = new StreamReader(_ns);
+                //int count = int.Parse(_num.ReadLine());
+                //MessageBox.Show(count.ToString(),"Number of Images found in the file");
+                //StreamReader _sr = new StreamReader(_ns);
+                //string[] _arr = new string[count];
+                //for (int i = 0; i < count; ++i)
+                //    _arr[i] = _sr.ReadLine();
+
+                //for (int i = 0; i < count; i++)
+                //    MessageBox.Show(_arr[i], "Client | paths of images ");
+
+                _client.Close();
+                
+            }
+            catch (Exception ep)
+            { MessageBox.Show(ep.Message,"Client");}
+        }
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream _ms = new MemoryStream(byteArrayIn);
+            Image _returnImage = Image.FromStream(_ms);
+            return _returnImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- server_s_thread/server_s_thread/Form1.Designer.cs
+++ server_s_thread/server_s_thread/Form1.Designer.cs
@@ -28,21 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label1.Location = new System.Drawing.Point(128, 138);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server is Here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(379, 322);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
